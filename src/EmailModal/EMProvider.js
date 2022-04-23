e562a70a6fc1c568d4f447a67e73d81ba33e7155
keyservlad <arnaud.guilhamat@isep.fr>
2022-04-23 19:53:31 -0400

    added global context with openModal true or false

---
+++ src/EmailModal/EMProvider.js
@@ -0,0 +1,20 @@
+import React, {useContext, useState} from 'react';
+
+export const StateContext = React.createContext();
+
+export function useStateContext() {
+    return useContext(StateContext)
+}
+
+
+export function EMProvider({children}){
+    const [newState, setNewState] = useState({
+        openModal: true
+    })
+
+    return (
+        <StateContext.Provider value={newState}>
+            {children}
+        </StateContext.Provider>
+    )
+}
